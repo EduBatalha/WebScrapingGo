52bb5a038a407ce035db5d6fbe2e9d12ca92c6cc
EduBatalha <eduardobatalha25@outlook.com>
2024-05-05 23:19:53 -0300

    scraping funcionando legalzinho

--- main.go
+++ main.go
@@ -8,5 +8,6 @@ import (
 func main() {
 	scraperService := services.NewScraper()
 	controller := controllers.NewProductController(scraperService)
+	//controller.FetchAndDisplayProduct("https://www.canecadodev.com/caneca-go-lang-commands-cheat-sheet-preta")
 	controller.FetchAndDisplayProduct("https://sleepcalm.com.br/produto/colchao-plus")
 }
--- services/scraperService.go
+++ services/scraperService.go
@@ -13,20 +13,38 @@ func NewScraper() *Scraper {
 }
 
 func (s *Scraper) FetchProductCode(url string) (*models.Product, error) {
+    log.Println("Conectando ao navegador...")
     browser := rod.New().MustConnect()
     defer browser.MustClose()
 
+    log.Printf("Abrindo página: %s\n", url)
     page := browser.MustPage(url)
 
-    // Espera até que o elemento esteja pronto
-    el := page.MustWaitLoad().MustElement("[data-trustvox-product-code]")
-
-    // Obtém o valor do atributo data-trustvox-product-code
-    productCode, err := el.Attribute("data-trustvox-product-code")
+    log.Println("Esperando até que um dos elementos esteja pronto...")
+    // Espera até que um dos elementos esteja pronto
+    el, err := page.MustWaitLoad().Element("[data-trustvox-product-code], [data-trustvox-product-code-js]")
     if err != nil {
-        log.Printf("Erro ao obter o atributo data-trustvox-product-code: %v", err)
+        log.Printf("Erro ao esperar pelo elemento: %v", err)
         return nil, err
     }
 
-    return &models.Product{NewId: *productCode}, nil
+    var productCode string
+
+    log.Println("Obtendo o valor do atributo data-trustvox-product-code...")
+    attr, err := el.Attribute("data-trustvox-product-code")
+    if err == nil && attr != nil {
+        productCode = *attr
+    } else {
+        log.Println("Não foi possível obter o atributo data-trustvox-product-code, tentando data-trustvox-product-code-js...")
+        attr, err = el.Attribute("data-trustvox-product-code-js")
+        if err == nil && attr != nil {
+            productCode = *attr
+        } else {
+            log.Printf("Erro ao obter o atributo data-trustvox-product-code-js: %v", err)
+            return nil, err
+        }
+    }
+
+    log.Println("Produto encontrado com sucesso!")
+    return &models.Product{NewId: productCode}, nil
 }
