833642d2a228bccb421865127514eebb5db3a240
EduBatalha <eduardobatalha25@outlook.com>
2024-05-07 23:22:33 -0300

    ajuste retorno

--- main.go
+++ main.go
@@ -1,16 +1,38 @@
 package main
 
 import (
-
+	"fmt"
+	"os"
 	"WebScrapingGo/controllers"
 	"WebScrapingGo/services"
-	
+	"WebScrapingGo/views"
 )
 
 func main() {
+	    // Check if the necessary command line arguments are provided
+		if len(os.Args) < 4 {
+			fmt.Println("Uso: <executável> <storeID> <productID> <storeToken>")
+			return
+		}
+	
+		// Extract command line arguments
+		storeID := os.Args[1]
+		productID := os.Args[2]
+		storeToken := os.Args[3]
+	
+		// Retrieve the product details from the Trustvox API
+		product, err := services.RetrieveOldProduct(storeID, productID, storeToken)
+		if err != nil {
+			views.DisplayError(err)
+			return
+		}
+	
 	// Inicialização do serviço de scraping e do controller
 	scraperService := services.NewScraper()
 	controller := controllers.NewProductController(scraperService)
 	controller.FetchAndDisplayProduct("https://www.canecadodev.com/caneca-go-lang-commands-cheat-sheet-preta")
 	//controller.FetchAndDisplayProduct("https://sleepcalm.com.br/produto/colchao-plus")
+
+	// Display the retrieved product details
+	views.DisplayProduct(product)
 }
